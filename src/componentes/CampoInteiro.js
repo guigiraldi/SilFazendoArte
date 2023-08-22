d533e5310c8d7dd60d73c1a502de0807a5b91bbf
Guga <113147888+GugaAraujo1@users.noreply.github.com>
2023-08-22 19:04:08 -0300

    Implementação do componente CampoInteiro.js

---
+++ src/componentes/CampoInteiro.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import Texto from './Texto';
+
+export default function CampoInteiro() {
+  return <>
+    <TextInput value = '0'/>
+  </>
+}
--- src/telas/Produtos/Item.js
+++ src/telas/Produtos/Item.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, TextInput } from "react-native";
 import { Button } from "react-native-elements";
 import Texto from "../../componentes/Texto";
+import CampoInteiro from "../../componentes/CampoInteiro";
 import estilos from "./estilos";
 
 export default function Item({ nome, descricao, preco }) {
@@ -14,7 +15,7 @@ export default function Item({ nome, descricao, preco }) {
         <View style={estilos.listadesejos}>
             <View style={estilos.posicao}>
                 <Texto>Quantidade: </Texto>
-                <TextInput value = '0'/>
+                <CampoInteiro/>
             </View>
             <View style={estilos.posicao}>
                 <Texto>Preço: </Texto>
